d14a732441e951a34fcf2497554bfc338c0103cb
CarlosBanda <c.banda07@hotmail.com>
2022-10-21 12:05:52 -0500

    update view register

--- src/components/panel.js
+++ src/components/panel.js
@@ -1,16 +1,36 @@
-import React from 'react'
-import {Text,StyleSheet,View, Pressable} from 'react-native'
+import React, { useState } from 'react'
+import {Text,StyleSheet,View, Pressable, Modal, Button} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 import Swiper from 'react-native-swiper'
 import Card from './card';
+import { TextInput } from 'react-native-gesture-handler';
 
 function Panel() {
+    const [isVisible, setIsVisible] = useState(false)
     return ( 
         <View style={styles.container}>
             <View style={styles.body}>
                 <View style={styles.addDevice}>
-                    <Pressable style={styles.btnAddDevice}>
-                        <Text style={{color:'black'}}>Agregar Dispositivo</Text>
+                    <Pressable style={styles.btnAddDevice} onPress={() =>{setIsVisible(true)}}>
+                        <Text style={{color:'black', alignItems:'center'}}>Agregar Dispositivo <Icon name='add-outline' size={25} color="blue"/></Text>
                     </Pressable>
+
+                    <Modal transparent={true} visible={isVisible}>
+                        <View style={styles.modalAdd}>
+                            <View style={styles.bodyModal}>
+                                <View style={styles.formModal}>
+                                    <Text style= {[styles.text, styles.textTitle]}>Nuevo Dispositivo</Text>
+                                    <Text style= {[styles.text, styles.textNum]}>Número de SIM</Text>
+                                    <TextInput style={styles.input} placeholderTextColor="#000" keyboardType='number-pad'
+                            maxLength={10} placeholder='Número' />
+                                </View>
+                                <View style={styles.btns}>
+                                    <Button color="red"  title='Cancelar' onPress={() => setIsVisible(false)}></Button>
+                                    <Button title='Agregar' onPress={() => setIsVisible(false)}></Button>
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
                 </View>
                 <Swiper style={styles.wrapper} showsButtons={true} >
                     <Card style={styles.card}/>
@@ -22,6 +42,31 @@ function Panel() {
 }
 
 const styles = StyleSheet.create({
+    input:{
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 20,
+        borderColor: '#d5d9dc',
+        borderWidth: 2,
+        color: '#000',
+        marginTop: 10,
+        marginBottom: 30,
+        fontSize: 15,
+        marginHorizontal: 10
+    },
+    textTitle:{
+        marginTop: 20,
+        fontSize: 15,
+        marginHorizontal: 80,
+        fontWeight: 'bold'
+    },
+    textNum:{
+        marginLeft: 20,
+        marginTop: 20
+    },
+    text:{
+        color: 'black'
+    },
     container:{
         flex: 1,
         backgroundColor: 'white'
@@ -63,6 +108,24 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         marginBottom: 20,
         borderColor: '#F5232D'
+    },
+    modalAdd:{
+        flex: 1,
+        backgroundColor:'rgba(0,0,0,0.3)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bodyModal:{
+        borderRadius: 10,
+        width: 300,
+        height: 250,
+        backgroundColor: 'white',
+        // alignItems: 'center',
+    },
+    btns:{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        marginHorizontal: 30
     }
 })
 export default Panel;
--- src/routes/Navigations.js
+++ src/routes/Navigations.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import Login from '../components/login';
 import Panel from '../components/panel';
 import Registro from '../components/register';
@@ -8,22 +9,27 @@ import Inputs from '../components/inputsLogin';
 import { EstadoCuenta } from '../components/estadoCuenta';
 import { Recargas } from '../components/recargas';
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 const Navigations = () => {
   return (
-          <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Login" component={Login}/>
-            <Stack.Screen name="Panel" component={Panel} />
-            <Stack.Screen name="Inputs" component={Inputs} />
-            <Stack.Screen name="Registro" component={Registro} />
-          </Stack.Navigator>
+
+          // <Stack.Navigator screenOptions={{headerShown: false}}>
+          //   <Stack.Screen name="Login" component={Login}/>
+          //   <Stack.Screen name="Panel" component={Panel} />
+          //   <Stack.Screen name="Inputs" component={Inputs} />
+          //   <Stack.Screen name="Registro" component={Registro} />
+          // </Stack.Navigator>
            
           //TODO: validacion user para mostrat bottom navigation
-          //  <Tab.Navigator screenOptions={{headerShown: false}}>
-          //   <Tab.Screen name="Panel"  component={Panel} />
-          //   <Tab.Screen name="EstadoCuenta" component={EstadoCuenta} />
-          //   <Tab.Screen name="Recargas" component={Recargas} />
-          // </Tab.Navigator> 
+           <Tab.Navigator screenOptions={{headerShown: false}}>
+            <Tab.Screen name="Inicio"  component={Panel} options={{
+              tabBarIcon:()=>(
+                <Icon name='house' size={25} color={'black'}/>
+              )
+            }}/>
+            <Tab.Screen name="Estado de Cuenta" component={EstadoCuenta} />
+            <Tab.Screen name="Recargas" component={Recargas} />
+          </Tab.Navigator> 
   );
 };
 
