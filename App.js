37ad7b90a4bdf9c9473c9059b3d06434a780338b
CarlosBanda <c.banda07@hotmail.com>
2022-10-22 13:29:41 -0500

    Obteniendo AsyncStorage e ingresar a la vista de productos

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   View,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigations from './src/routes/Navigations';
+import NavigationsLogged from './src/routes/NavigationLogged';
+import Register from './src/components/register';
 const App = () => {
 
+  const [dato, setDato] = useState(false);
   return (
       <View style={styles.container}>
         <NavigationContainer>
-          <Navigations/>
+          { !dato ? (
+            <Navigations/>
+          ):(
+            <NavigationsLogged />
+          )}
         </NavigationContainer>
       </View>
   );
--- src/routes/NavigationLogged.js
+++ src/routes/NavigationLogged.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -8,21 +8,21 @@ import Registro from '../components/register';
 import Inputs from '../components/inputsLogin';
 import { EstadoCuenta } from '../components/estadoCuenta';
 import { Recargas } from '../components/recargas';
-
-
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-const Navigations = () => {
+const NavigationsLogged = ({dato}) => {
+  return (
 
-  return ( 
-
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="Panel" component={Panel} />
-      <Stack.Screen name="Inputs" component={Inputs} />
-      <Stack.Screen name="Registro" component={Registro} />
-    </Stack.Navigator>
+           <Tab.Navigator screenOptions={{headerShown: false}}>
+            <Tab.Screen name="Inicio"  component={Panel} options={{
+              tabBarIcon:()=>(
+                <Icon name='house' size={25} color={'black'}/>
+              )
+            }}/>
+            <Tab.Screen name="Estado de Cuenta" component={EstadoCuenta} />
+            <Tab.Screen name="Recargas" component={Recargas} />
+          </Tab.Navigator> 
   );
 };
 
-export default Navigations;
+export default NavigationsLogged;
