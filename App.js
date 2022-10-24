792a1232d824e5339a5f73e38c0006a09c3034cd
CarlosBanda <c.banda07@hotmail.com>
2022-10-24 13:55:24 -0500

    Verificacion de accesos

--- App.js
+++ App.js
@@ -7,18 +7,22 @@ import { NavigationContainer } from '@react-navigation/native';
 // import Navigations from './src/routes/Navigations';
 import NavegationLogged from './src/routes/NavegationLogged';
 import Register from './src/components/register';
+import Navigations from './src/routes/Navigations';
 const App = () => {
-  // const [dato, setDato] = useState(false);
-  const dato = true
+  const [dato, setDato] = useState(false);
+  // const dato = true
+
   return (
     <View style={styles.container}>
     <NavigationContainer>
-      { !dato ? (
+      { dato ? (
         <Navigations/>
       ):(
         <NavegationLogged />
       )}
-    </NavigationContainer>
+
+
+    </NavigationContainer>      
   </View>
   );
 };
